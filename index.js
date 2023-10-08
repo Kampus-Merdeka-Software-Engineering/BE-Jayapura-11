9c50708dcfbd86ae1787569ce9c087d93aefd732
Jovan <jovanaan12@gmail.com>
2023-10-08 23:15:46 +0700

    update index route

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 require("./contactUs/route/route")(app);
+require("./course/route/route")(app);
 
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
